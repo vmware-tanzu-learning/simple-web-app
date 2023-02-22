6a320073fca3a76339c47bb8f53d26f58e3d279e
Neil Winton <nwinton@vmware.com>
2023-02-22 15:12:03 +0000

    Add logging, /status and more configuration

--- README.md
+++ README.md
@@ -8,15 +8,20 @@ Boot, so start-up times are very small.
 
 It provides the following endpoints:
 
-* `/` - just responds with `OK`
-* `/hello` - responds with a canned greeting message, which
-  can be over-ridden using the `GREETING_MESSAGE` environment
-  variable.
-* `/env` - responds with a JSON map of current environment variable settings
+* `/` - just responds with `OK`, but can be over-ridden using
+  the `OK_MESSAGE` environment variable.
 * `/echo` - a GET to this with a query-parameter of `message`
   echoes back the supplied message
 * `/echo` - a POST to this echoes back the body contents
-* `/actuator` - exposes the full range of actuator endpoints
+* `/env` - responds with a JSON map of current environment variable settings
+* `/hello` - responds with a canned greeting message, which
+  can be over-ridden using the `HELLO_MESSAGE` environment
+  variable.
+* `/status/<CODE>` - response with a status of the given code
+  value, and matching message.
 
-The liveness and readiness endpoints are also exposed on
-`/livez` and `/readyz`, respectively.
+In addition:
+
+* `/actuator` - exposes the full range of actuator endpoints
+* Liveness and readiness endpoints are also exposed on `/livez`
+  and `/readyz`, respectively.
--- src/main/java/com/vmware/tanzu/simplewebapp/SimpleController.java
+++ src/main/java/com/vmware/tanzu/simplewebapp/SimpleController.java
@@ -1,6 +1,11 @@
 package com.vmware.tanzu.simplewebapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -8,28 +13,41 @@ import java.util.Map;
 @RestController
 public class SimpleController {
 
+    Logger logger = LoggerFactory.getLogger(SimpleController.class);
+
     @GetMapping("/")
-    public String ok() {
-        return "OK";
+    public String ok(@Value("${ok.message:OK}") String message) {
+        logger.info("Requested / requested");
+        return message;
     }
 
     @GetMapping("/hello")
-    public String hello(@Value("${greeting.message:Hello from the Simple Web App!}") String message) {
+    public String hello(@Value("${hello.message:Hello from the Simple Web App!}") String message) {
+        logger.info("Requested /hello");
         return message;
     }
 
     @GetMapping("/env")
     public Map<String,String> env() {
+        logger.info("Requested /env");
         return System.getenv();
     }
 
     @GetMapping("/echo")
     public String echoParam(@RequestParam String message) {
+        logger.info("Requested /echo via GET");
         return message;
     }
 
     @PostMapping("/echo")
     public String echoBody(@RequestBody String body) {
+        logger.info("Requested /echo via POST");
         return body;
+    }
+
+    @GetMapping("/status/{code}")
+    public ResponseEntity<String> status(@PathVariable Integer code) {
+        logger.info("Requested /status/" + code);
+        return new ResponseEntity<>(HttpStatus.valueOf(code).getReasonPhrase(), HttpStatusCode.valueOf(code));
     }
 }
